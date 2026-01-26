ac270dd9b4685ebbabc14d2a899fec3e47fc01fa
Arkadiusz Szreiber <arkadiusz.szreiber@softmedica.pl>
2026-01-26 09:50:56 +0100

    Przeniesienie zmian z plików: * main.ts

--- src/app-public/main.ts
+++ src/app-public/main.ts
@@ -3,6 +3,7 @@ import { generateInvoice, generatePDFUPO } from '../lib-public';
 import { AdditionalDataTypes } from '../lib-public/types/common.types';
 
 const inputInvoice: HTMLInputElement = document.getElementById('xmlInput') as HTMLInputElement;
+const inputInvoiceOffline: HTMLInputElement = document.getElementById('xmlInputOffline') as HTMLInputElement;
 const inputUPO: HTMLInputElement = document.getElementById('xmlInputUPO') as HTMLInputElement;
 
 inputInvoice.addEventListener('change', async (): Promise<void> => {
@@ -16,6 +17,35 @@ inputInvoice.addEventListener('change', async (): Promise<void> => {
     nrKSeF: '5555555555-20250808-9231003CA67B-BE',
     qrCode:
       'https://qr-test.ksef.mf.gov.pl/invoice/5265877635/26-10-2025/HS5E1zrA8WVjDNq_xMVIN5SD6nyRymmQ-BcYHReUAa0',
+  };
+
+  generateInvoice(file, additionalData, 'blob').then((data: Blob): void => {
+    const url: string = URL.createObjectURL(data);
+
+    const a: HTMLAnchorElement = document.createElement('a');
+
+    a.href = url;
+    a.download = 'test.pdf';
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  });
+});
+
+inputInvoiceOffline.addEventListener('change', async (): Promise<void> => {
+  const file: File | undefined = inputInvoice.files?.[0];
+
+  if (!file) {
+    return;
+  }
+
+  const additionalData: AdditionalDataTypes = {
+    encodedFakturaURL:
+      'aHR0cHM6Ly9rc2VmLXRlc3QubWYuZ292LnBsL2NsaWVudC1hcHAvaW52b2ljZS81MjY1ODc3NjM1LzI2LTEwLTIwMjUvSFM1RTF6ckE4V1ZqRE5xX3hNVklONVNENm55UnltbVEtQmNZSFJlVUFhMA',
+    encodedCertyfikatURL:
+      'aHR0cHM6Ly9xci10ZXN0LmtzZWYubWYuZ292LnBsL2NlcnRpZmljYXRlL05pcC81NTUyMTQyNTQ1LzU1NTIxNDI1NDUvMDE5YmRlY2RkM2RlNTE2Ny9DRFRFankyRVo4emtjSVRnN09NeTE0Yi0wVUhzS0c4eVhLdUkyR3VWWkN3L01FUUNJRFpOZi1MeEpzM2tBZjRqN0l1dV9FUDlXNnd3bDJKTFNfMWVzVG5VSDlMSEFpQWlKdXhsX0hzSXFBYlM4UWdDUVJvVkVxVjhfZVVWRGhkYkM5SDVIcDAtSlE',
   };
 
   generateInvoice(file, additionalData, 'blob').then((data: Blob): void => {
