b7ee100b2854f7752040e50bddc42bd54813978a
Arkadiusz Szreiber <arkadiusz.szreiber@softmedica.pl>
2026-01-26 09:35:37 +0100

    Przeniesienie zmian z plików: * UPO-4_2-generators.ts

--- src/lib-public/UPO-4_2-generators.ts
+++ src/lib-public/UPO-4_2-generators.ts
@@ -4,11 +4,39 @@ import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { generateStyle } from '../shared/PDF-functions';
 import { generateNaglowekUPO } from './generators/UPO4_2/Naglowek';
 import { generateDokumnetUPO } from './generators/UPO4_2/Dokumenty';
-import { parseXML } from '../shared/XML-parser';
+import { parseXML, parseXMLStr } from '../shared/XML-parser';
 import { Position } from '../shared/enums/common.enum';
 
 export async function generatePDFUPO(file: File): Promise<Blob> {
   const upo = (await parseXML(file)) as Upo;
+  const docDefinition: TDocumentDefinitions = {
+    content: [generateNaglowekUPO(upo.Potwierdzenie!), generateDokumnetUPO(upo.Potwierdzenie!)],
+    ...generateStyle(),
+    pageSize: 'A4',
+    pageOrientation: 'landscape',
+    footer: function (currentPage: number, pageCount: number) {
+      return {
+        text: currentPage.toString() + ' z ' + pageCount,
+        alignment: Position.RIGHT,
+        margin: [0, 0, 20, 0],
+      };
+    },
+  };
+
+  return new Promise((resolve, reject): void => {
+    pdfMake.createPdf(docDefinition).getBlob((blob: Blob): void => {
+      if (blob) {
+        resolve(blob);
+      } else {
+        reject('Error');
+      }
+    });
+  });
+}
+
+
+export async function generatePDFUPOString(xmlString: string): Promise<Blob> {
+  const upo = (await parseXMLStr(xmlString)) as Upo;
   const docDefinition: TDocumentDefinitions = {
     content: [generateNaglowekUPO(upo.Potwierdzenie!), generateDokumnetUPO(upo.Potwierdzenie!)],
     ...generateStyle(),
