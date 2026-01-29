a7d0c685cbf2be61c050a9cd7dfcaa380de848f8
Arkadiusz Szreiber <arkadiusz.szreiber@softmedica.pl>
2026-01-29 10:24:20 +0100

    Generowanie potwierdzenia transakcji * Dodano obsługę braku identyfikatora podatkowego lub identyfikatora innego niż NIP oraz w danych nabywcy

--- src/lib-public/generators/confirmation/DaneNabywcy.ts
+++ src/lib-public/generators/confirmation/DaneNabywcy.ts
@@ -5,23 +5,59 @@ import FormatTyp from '../../../shared/enums/common.enum';
 
 export function generateDaneNabywcy(daneNabywcy: Podmiot2): Content[] {
   const result: Content[] = createHeader('Dane nabywcy', [0, 8, 0, 0]);
+  const { DaneIdentyfikacyjne } = daneNabywcy;
 
-  if (daneNabywcy.DaneIdentyfikacyjne) {
-    result.push(
-      {
-        text: [
-          formatText('NIP: ', FormatTyp.HeaderContent),
-          formatText(daneNabywcy.DaneIdentyfikacyjne.NIP?._text, null, { fontSize: 10 }) as ContentText,
-        ],
-      },
-      {
-        text: [
-          formatText('Nazwa: ', FormatTyp.HeaderContent),
-          formatText(daneNabywcy.DaneIdentyfikacyjne.Nazwa?._text, null, { fontSize: 10 }) as ContentText,
-        ],
-      }
-    );
+  if (!DaneIdentyfikacyjne) {
+    return result;
   }
+
+  if (DaneIdentyfikacyjne.NIP?._text) {
+    // <NIP>3766866819</NIP>
+    result.push({
+      text: [
+        formatText('NIP: ', FormatTyp.HeaderContent),
+        formatText(DaneIdentyfikacyjne.NIP?._text, null, { fontSize: 10 }) as ContentText,
+      ],
+    });
+  } else if (DaneIdentyfikacyjne.BrakID) {
+    // <BrakID>1</BrakID>
+    result.push({
+      text: [formatText('Brak identyfikatora podatkowego', FormatTyp.HeaderContent)],
+    });
+  } else if (DaneIdentyfikacyjne.NrVatUE) {
+    // <KodUE>DE</KodUE>
+    // <NrVatUE>12/345/67890</NrVatUE>
+    result.push({
+      text: [
+        formatText('Nr VAT UE: ', FormatTyp.HeaderContent),
+        formatText(`${DaneIdentyfikacyjne.KodUE?._text} ${DaneIdentyfikacyjne.NrVatUE?._text}`, null, {
+          fontSize: 10,
+        }) as ContentText,
+      ],
+    });
+  } else if (DaneIdentyfikacyjne.NrID) {
+    // <KodKraju>UG</KodKraju>
+    // <NrID>TIN 1021375648</NrID>
+    result.push({
+      text: [
+        formatText('Nr ID: ', FormatTyp.HeaderContent),
+        formatText(
+          `${DaneIdentyfikacyjne.KodKraju ? DaneIdentyfikacyjne.KodKraju._text + ' ' : ''}${DaneIdentyfikacyjne.NrID?._text}`,
+          null,
+          {
+            fontSize: 10,
+          }
+        ) as ContentText,
+      ],
+    });
+  }
+
+  result.push({
+    text: [
+      formatText('Nazwa: ', FormatTyp.HeaderContent),
+      formatText(DaneIdentyfikacyjne.Nazwa?._text, null, { fontSize: 10 }) as ContentText,
+    ],
+  });
 
   if (daneNabywcy.Adres && (daneNabywcy.Adres.AdresL1 || daneNabywcy.Adres.AdresL2)) {
     const line1 = daneNabywcy.Adres.AdresL1 ?? daneNabywcy.Adres.AdresL2;
