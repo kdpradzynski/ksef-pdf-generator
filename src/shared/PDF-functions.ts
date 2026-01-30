48242ea81e040b696542697a3ba3a77cd66a9e92
Michał Chudy <michal.chudy@akmf.pl>
2026-01-30 20:06:43 +0100

    chore(): 1.0.0
    (cherry picked from commit f48680a65f5601b7e00ccb28b21a28fc7b3c3364)

--- src/shared/PDF-functions.ts
+++ src/shared/PDF-functions.ts
@@ -534,7 +534,7 @@ export function getKraj(code: string): string {
   return code;
 }
 
-export function getTStawkaPodatku(code: string, version: 1 | 2 | 3): string {
+export function getTStawkaPodatku(code: string, version: 1 | 2 | 3, P_PMarzy?: string): string {
   let TStawkaPodatkuVersioned: Record<string, string> = {};
 
   switch (version) {
@@ -547,6 +547,9 @@ export function getTStawkaPodatku(code: string, version: 1 | 2 | 3): string {
     case 3:
       TStawkaPodatkuVersioned = TStawkaPodatku_FA3;
       break;
+  }
+  if (!code && P_PMarzy === '1') {
+    return 'marża';
   }
 
   if (TStawkaPodatkuVersioned[code]) {
